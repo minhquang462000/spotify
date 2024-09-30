61d10fd75f2833040d0d399354f3e2d4fb2a6729
minhquang462000 <nguyenminhquang.cd2017@gmail.com>
2024-09-30 16:29:14 +0700

    ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps:[
+        {
+            name:"SPOTIFY-API",
+            script:'./app.js',
+            evn:{
+                 "NODE_ENV":"production",
+                "PORT":1001,
+            }
+        }
+    ]
+}
